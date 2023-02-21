d9caab2df7a38565a4a834bbb9e51a5ce3699d6a
heyIamThoong <heyiamthoong@gmail.com>
2023-02-21 10:42:40 +0700

    feact : update link

--- src/admin/ProjectListAdmin.js
+++ src/admin/ProjectListAdmin.js
@@ -42,7 +42,7 @@ const projectListAdmin = ()=>{
             <td><img style="width : 180px" class="img-fluid card-img-top" src="${item.gallery}" alt="2-start-simple"/></td>
             <td>${item.describe}</td>
             <td>
-            <button onclick="return confirm_delete()" data-id=${item.id} class="bg-red-600 p-5 py-2 text-[#ffff] btn_delete">DELETE</button>
+            <button onclick="return confirm_delete()" data-id=${item.id} class="bg-red-600 p-2 py-2 text-[#ffff] btn_delete">DELETE</button>
             <a class="chan" href="/admin/projectEditAdmin/${item.id}" class="bg-blue-900 p-5 py-2 text-[#ffff]">SETTING</a>
             </td>
         </tr>
